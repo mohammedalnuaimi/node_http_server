cf1a487840f28d4c7dd608e3e9e6763f08706f28
Mohammed A <mohammeda@Mohammeds-MacBook-Pro.local>
2019-02-18 00:48:32 +0000

    add promoise all

--- http_server.js
+++ http_server.js
@@ -6,18 +6,36 @@ const port = 8080;
 const server = http.createServer((req, res) => {
     console.log(req.url)
 
-    const promisfy = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            return resolve('hey I came back after 5000')
-        }, 5000);
+    const promisfy = () => {
+        console.log(`this is my js promise to learn in 5000`)
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, 5000);
+        })
+    }
+
+    // promisfy().then(() => {
+    //     return promisfy()
+    // }).then(() => {
+    //     return promisfy()
+    // }).then(() => {
+    //     return promisfy()
+    // }).then(data => {
+    //     console.log(`this is my js promise finished ${data}`)
+    // })
+
+    Promise.all([
+        promisfy(),
+        promisfy(),
+        promisfy(),
+        promisfy()
+    ]).then(() => {
+        console.log(`this is my js promise finished`)
     })
 
-    promisfy.then( data =>{
-        console.log(`this is my js promise to learn ${data}`)
-    })
 
-    res.setHeader('Content-Type' , 'image/jpg')
-    
+
+    res.setHeader('Content-Type', 'image/jpg')
+
     fs.readFile(path.resolve(__dirname, 'mosul_bridge.jpg'), (err, data) => {
         res.end(data)
     })
